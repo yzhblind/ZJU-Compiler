82782cb98021cb5fd96fdaf3d13d8599cbb73ddc
yzhblind <yangzihan2000@outlook.com>
2022-05-28 23:37:19 +0800

    minor change

--- src/ast/ast_stmt.cpp
+++ src/ast/ast_stmt.cpp
@@ -1,2 +1,42 @@
 #include "ast_stmt.hpp"
 
+ASTActualPara::ASTActualPara(ASTExpr *expr)
+{
+    
+}
+
+ASTActualPara::ASTActualPara(char *id, bool is_func)
+{
+    
+}
+
+ASTActualPara::~ASTActualPara()
+{
+    
+}
+
+ASTActualPara::TypeKind ASTActualPara::get_type()
+{
+    
+}
+
+ASTActualPara* ASTActualPara::append(ASTActualPara *next)
+{
+    
+}
+
+ASTReadPara::ASTReadPara(ASTVarAccess *start)
+{
+    
+}
+
+ASTReadPara::~ASTReadPara()
+{
+    
+}
+
+void ASTReadPara::push_back(ASTVarAccess *next)
+{
+    
+}
+
