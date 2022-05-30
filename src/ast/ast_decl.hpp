5fa63e5d9dddaab7ed2e7ec43e36fa9ee0a1b306
yzhblind <yangzihan2000@outlook.com>
2022-05-31 01:14:06 +0800

    add some comments

--- src/ast/ast_decl.hpp
+++ src/ast/ast_decl.hpp
@@ -7,7 +7,7 @@ using std::string;
 
 #include <vector>
 using std::vector;
-
+// 常量值节点，因为各种常量值所占空间都很小，选择直接将其放置于一个节点内
 class ASTConstValue : public ASTNode
 {
 public:
@@ -18,31 +18,35 @@ public:
     ~ASTConstValue();
     enum ConstType
     {
-        INT,
-        REAL,
-        STRING,
-        ID,
-        NIL
+        INT, //整数，当int64处理
+        REAL, //浮点数，当double处理
+        STRING, //字符串
+        ID, //标识符
+        NIL //空指针
     };
+    // 表示当前常量值的类型
     ConstType value_type;
     int64_t int_value;
     double real_value;
     string str;
 };
-
+// 常量定义节点，每个常量定义节点的结构都是将某个常量值定义为标识符
 class ASTConstDef : public ASTNode
 {
 public:
     ASTConstDef(char *id, ASTConstValue *value);
     ~ASTConstDef();
     ASTConstDef *append(ASTConstDef *next);
+    // 标识符
     string id;
+    // 常量值
     ASTConstValue *value;
+    // 如果不为nullptr说明当前常量定义区还未结束
     ASTConstDef *next_const_def;
 };
 
 class ASTVarDecl;
-
+// 定义类型节点，类型类是纯虚类，只作为指针使用
 class ASTType : public ASTNode
 {
 public:
@@ -55,6 +59,7 @@ public:
         RECORD,
         POINTER
     };
+    // 表示当前子类具体是什么类型，和dynamic cast用于判断类型时用途重合但会更方便一点？
     virtual ASTType::TypeKind get_type() = 0;
 };
 
@@ -64,9 +69,10 @@ public:
     ASTTypeId(char *id);
     ~ASTTypeId();
     ASTType::TypeKind get_type();
+    // 类型本身可以是另一个类型标识符
     string id;
 };
-
+// range类型，在我们简化过的pascal的语法中唯一的作用是定义数组下标范围
 class ASTTypeSubrange : public ASTType
 {
 public:
@@ -74,8 +80,12 @@ public:
     ASTTypeSubrange(ASTConstValue *left, ASTConstValue *right);
     ~ASTTypeSubrange();
     ASTType::TypeKind get_type();
+    // range有两种，一种是代表range的标识符，一种是直接的定义
+    // 前者最终还是要归结到后者
     bool is_range_id;
+    // true：使用string读取标识符
     string range_id;
+    // false：直接读取上下界，注意检查常量值的类型是否合法
     ASTConstValue *left, *right;
 };
 
--- src/ast/ast_prgm.hpp
+++ src/ast/ast_prgm.hpp
@@ -3,15 +3,20 @@
 #include "ast_all.hpp"
 
 #include <string>
-
+// 根节点，整个程序的入口
 class ASTRoot : public ASTNode
 {
 public:
     ASTRoot(ASTConstDef *const_def, ASTTypeDef *type_def, ASTVarDecl *var_decl, ASTProcFuncDecl *proc_func_decl, ASTStmt *stmt);
     ~ASTRoot();
+    // 常量定义
     ASTConstDef *const_def;
+    // 类型定义
     ASTTypeDef *type_def;
+    // 变量声明
     ASTVarDecl *var_decl;
+    // 可能为nullptr
     ASTProcFuncDecl *proc_func_decl;
+    // 程序主体部分
     ASTStmt *stmt;
 };
